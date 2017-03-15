e9121584c9c687a50c7dd64dbe7cbcd6958828c6
dnuridhuha <dwiki.nuridhuha@outlook.com>
2017-03-15 11:19:10 +0700

    Signed-off-by: dnuridhuha <dwiki.nuridhuha@outlook.com>
    memasukan kodingan awal

---
+++ tugas_1.cpp
@@ -0,0 +1,14 @@
+// tugas_1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h" //hanya untuk visual studio saja
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
+
