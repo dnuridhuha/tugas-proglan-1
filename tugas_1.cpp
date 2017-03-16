f5db59a5cf604d784d802e88cbe9fc688ad39cb0
dwikinuridhuha <dwiki.nuridhuha@gmail.com>
2017-03-16 20:43:01 +0700

    Signed-off-by: dwikinuridhuha <dwiki.nuridhuha@gmail.com>
    menghapus yang tidak penting dan menabah cout "hello"

--- tugas_1.cpp
+++ tugas_1.cpp
@@ -1,16 +1,10 @@
-// tugas_1.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h" //hanya untuk visual studio saja
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int a,b,c,d;
-    char a[10];
-    
+	cout << "Hello" << endl;
     return 0;
 }
 
