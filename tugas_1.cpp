b237952f62a81d5fe3410f5228dbd4a222a742b6
zakki-af <zakki.ahmad.fauzi@gmail.com>
2017-03-18 17:05:39 +0700

    Update tugas_1.cpp

--- tugas_1.cpp
+++ tugas_1.cpp
@@ -38,7 +38,7 @@ class buku {
 //Dari Dwiki
 void daftarTeknologi(){
 	//membuat variabel class array teknologi berisi 5
-	buku teknologi[5];
+	buku teknologi[5],sejarah[5],politik[5];
 	
 	//membuat mengisi array 'teknologi'
 	teknologi[0].isi_buku("Programing C++", "Dwiki Nuridhuha", "Ini buku tentang pemrograman C++", "Informatika", "Mojokerto", 627, 2018);
@@ -53,6 +53,45 @@ void daftarTeknologi(){
 	for (int i = 0; i < sizeof(teknologi) / sizeof(buku); i++) {
 		cout << "\nDaftar yang ke-" << i + 1 << endl;
 		teknologi[i].tampil();
+	}
+}
+//Dari Zakki
+void daftarSejarah(){
+	//membuat variabel class array sejarah berisi 5
+	buku sejarah[5];
+	
+	//membuat mengisi array 'sejarah'
+	teknologi[0].isi_buku("Api Sejarah Jilid Kesatu", "Ahmad Mansur Suryanegara", "Ini buku tentang sejarah Indonesia yang sebenarnya", "Salamadani Pustaka Semesta", "Bandung", 584, 2009);
+	teknologi[1].isi_buku("Api Sejarah Jilid Kedua", "Ahmad Mansur Suryanegara", "Ini buku tentang sejarah Indonesia yang sebenarnya", "Salamadani Pustaka Semesta", "Bandung", 578, 2010);
+	teknologi[2].isi_buku("Menemukan Sejarah:wacana pergerakan Islam di Indonesia", "Ahmad Mansur Suryanegara", "Ini buku tentang wacana pergerakan Islam di Indonesia", "Mizan", "Jakarta", 335, 1995);
+	teknologi[3].isi_buku("", "Ahmad Mansur Suryanegara", "Ini buku tentang membuat Mobile App", "Informatika", "Mojokerto", 675, 1995);
+	teknologi[4].isi_buku("Pemberontakan Tentara Pembela Tanah Air- Peta di Cileunca Pangalengan Bandung Selatan", "Ahmad Mansur Suryanegara", "Ini buku tentang programing python", "Informatika", "Mojokerto", 497, 1996);
+	
+	cout << "Daftar Buku Sejarah :" << endl;
+	
+	//menapilkan daftar buku sejarah
+	for (int i = 0; i < sizeof(sejarah) / sizeof(buku); i++) {
+		cout << "\nDaftar yang ke-" << i + 1 << endl;
+		sejarah[i].tampil();
+	}
+}
+void daftarPolitik(){
+	//membuat variabel class array sejarah berisi 5
+	buku politik[5];
+	
+	//membuat mengisi array 'politik'
+	teknologi[0].isi_buku("Programing C++", "Dwiki Nuridhuha", "Ini buku tentang pemrograman C++", "Informatika", "Mojokerto", 627, 2018);
+	teknologi[1].isi_buku("Become Hakel", "Dwiki Nuridhuha", "Ini buku tentang Hakel", "Informatika", "Mojokerto", 482, 2020);
+	teknologi[2].isi_buku("Non Blocking With NodeJS", "Dwiki Nuridhuha", "Ini buku tentang membuat Website", "Informatika", "Mojokerto", 524, 2019);
+	teknologi[3].isi_buku("Membuat Hybrid App dengan Ionic", "Dwiki Nuridhuha", "Ini buku tentang membuat Mobile App", "Informatika", "Mojokerto", 675, 2020);
+	teknologi[4].isi_buku("Data sains dengan Python", "Dwiki Nuridhuha", "Ini buku tentang programing python", "Informatika", "Mojokerto", 497, 2021);
+	
+	cout << "Daftar Buku Politik :" << endl;
+	
+	//menapilkan daftar buku politik
+	for (int i = 0; i < sizeof(politik) / sizeof(buku); i++) {
+		cout << "\nDaftar yang ke-" << i + 1 << endl;
+		politik[i].tampil();
 	}
 }
 
