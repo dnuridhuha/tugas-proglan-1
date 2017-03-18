cde8100da990d88aca34f2fb561e41cae06464dd
Yulius Pratama <yuliuspratama9e@gmail.com>
2017-03-18 11:19:30 +0700

    Update 0.5
    penambahan buku list dan memperbarui tampilan

--- tugas_1.cpp
+++ tugas_1.cpp
@@ -1,7 +1,7 @@
 //agama, fiksi = rahma
 //zakki = sejarah, politik
 // dwiki = teknologi, classnya
-// yulias = psiko
+// yulias = psiko ,
 // mei = filsa
 
 #include <iostream>
@@ -12,7 +12,7 @@ using namespace std;
 class buku {
 	string judul, penulis, deskripsi, penerbit;
 	int hal, tahun;
-	
+
 	public:
 		void isi_buku(string judul, string penulis, string deskripsi, string penerbit, int hal, int tahun){
 			this->hal = hal;
@@ -22,7 +22,7 @@ class buku {
 			this->judul = judul;
 			this->tahun = tahun;
 		}
-		
+
 		void tampil(){
 			cout << "Judul\t:" << this->judul << endl;
 			cout << "Halaman\t:" << this->hal << endl;
@@ -34,16 +34,38 @@ class buku {
 
 int main()
 {
+
+    cout << "==================================================" << endl;
+    cout << "=                 LIST DATA BUKU                 =" << endl;
+    cout << "=                   THE DREAMER TEAM             =" << endl;
+    cout << "==================================================" << endl;
+    cout << endl;
+    cout << endl;
+    cout << "=                 The Technology book            =" << endl;
+    cout << endl;
 	buku teknologi[2];
-	
+
 	teknologi[0].isi_buku("Program C++", "Dwiki", "Ini buku tentang pemrograman C++", "Informatika", 627, 2018);
 	teknologi[0].tampil();
-	
+
 	cout << endl;
-	
+
 	teknologi[1].isi_buku("Become Hakel", "Dwiki", "Ini buku tentang Hakel", "Informatika", 482, 2020);
 	teknologi[1].tampil();
-	
+
+    cout << endl;
+    cout << "=                 The Psychology book            =" << endl;
+    cout << endl;
+
+	buku psychology[2];
+
+	psychology[0].isi_buku("Belajar Bahasa Tubuh", "Yulius", "Buku ini mengajarkan cara beljar bahasa tubuh yang baik  ", "YuliusPratama", 158, 2030);
+	psychology[0].tampil();
+
+	cout << endl;
+
+	psychology[1].isi_buku(" Read Mind ", "Yulius", "Ini buku tentang bagaimana membaca pikiran Manusia ", "YuliusPratama", 382, 2029);
+	psychology[1].tampil();
     return 0;
 }
 
