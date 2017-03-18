7e212dfbcea635ad21da8cf51a59de9f7c7e95c7
ohohrahma <ohohrahma@gmail.com>
2017-03-18 18:16:22 +0700

    Update tugas_1.cpp
    Dari rahma sudah

--- tugas_1.cpp
+++ tugas_1.cpp
@@ -98,23 +98,68 @@ void daftarTeknologi(){
 	}
 }
 
+//Dari Rahma
+void daftarAgama(){
+	//membuat variabel class array Agama berisi 5
+	buku agama[5];
+	
+	//membuat mengisi array 'Agama'
+	agama[0].isi_buku("Keajaiban Belajar Al-Qur'an", "Haya Ar-Rosyid", "Meraih kemuliaan bersama Al-Qur'an'", "Darul Wathon Lin Nasyr", "Jakarta", 181, 2007);
+	agama[1].isi_buku("Fiqh Ibadah", "H.Tolhah Ma'ruf'", "Panduan lengkap beribadah versi ahlussunnah", "Lembaga Ta'lif Wannasyr'", "Kediri", 332, 2000);
+	agama[2].isi_buku("Penghapus Dosa-Dosa Wanita", "Majdi Fathi As-Sayyid", "Menjelaskan penghapus dosa-dosa wanita", "Al-Maktabah At-Taufiqiyah", "Jakarta Timur", 251, 2002);
+	agama[3].isi_buku("Menjadi Cendekiawan Muslim", "DR.KH.Zakky Mubarak, MA", "Kuliah islam di perguruan tinggi", "PT Magenta Bhakti Guna", "Jakarta", 351, 2007);
+	agama[4].isi_buku("Fiqih Birrul Walidain", "Musthafa bin Al-'A'dawi'", "Menjemput surga dengan bakti orangtua", "Mktabah Makkah", "Jakarta", 290, 2002);
+	
+	
+	cout << "Daftar Buku Agama :" << endl;
+	
+	//menapilkan daftar buku Agama
+	for (int i = 0; i < sizeof(agama) / sizeof(buku); i++) {
+		cout << "\nDaftar yang ke-" << i + 1 << endl;
+		agama[i].tampil();
+	}
+}
+
+//Dari Rahma
+void daftarFiksi(){
+	//membuat variabel class array Fiksi berisi 5
+	buku fiksi[5];
+	
+	//membuat mengisi array 'Fiksi'
+	fiksi[0].isi_buku("Dilan", "Pidi Baiq", "Kisah Dilan tahun 1990", "PT Mizan Pustaka", "Bandung", 330, 2014);
+	fiksi[1].isi_buku("Dilan 2", "Pidi Baiq", "Kisah Dilan tahun 1990 jilid 2", "PT Mizan Pustaka", "Bandung", 342, 2015);
+	fiksi[2].isi_buku("With Julian", "Wulanfadi", "Mereka yang belajar untuk memaafkan dan dimaafkan", "Loveable", "Jakarta Selatan", 363, 2016);
+	fiksi[3].isi_buku("FriendZone", "Vanesa Marcella", "Seandainya kamu peka lebih awal", "Bentang Belia", "Yogyakarta", 169, 2016);
+	fiksi[4].isi_buku("Rubik's Cube", "Medina Savira", "4 sahabat yang menyukai rubik", "PT Mizan Pustaka", "Bandung", 168, 2011);
+	
+	
+	cout << "Daftar Buku Fiksi :" << endl;
+	
+	//menapilkan daftar buku Fiksi
+	for (int i = 0; i < sizeof(fiksi) / sizeof(buku); i++) {
+		cout << "\nDaftar yang ke-" << i + 1 << endl;
+		fiksi[i].tampil();
+	}
+}
+
 int main()
 {
 	cout << "==================================================" << endl;
-    cout << "=                 LIST DATA BUKU                 =" << endl;
-    cout << "=                   THE DREAMER TEAM             =" << endl;
-    cout << "==================================================" << endl;
-    cout << endl;
-    cout << endl;
+    	cout << "=                 LIST DATA BUKU                 =" << endl;
+    	cout << "=                   THE DREAMER TEAM             =" << endl;
+    	cout << "==================================================" << endl;
+    	cout << endl;
+    	cout << endl;
 	
 	daftarTeknologi(); //Dari Dwiki
 	
 	daftarFilsafat(); //Dari Meidiana
 	
+	daftarAgama(); // Dari Rahma
+	
+	daftarFiksi(); // Dari Rahma
+	
 	daftar_psychology(); // Dari Yulius
 	
-	
-	
-    return 0;
+    	return 0;
 }
-
